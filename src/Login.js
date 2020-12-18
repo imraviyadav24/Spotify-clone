6332de2e3fcaec879b442d4063abb37b29e6d0bf
ravi yadav <raviyadav00063@gmail.com>
2020-12-18 22:33:40 +0530

    Spotify Authentication is implemented

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Login.css';
+import { loginUrl } from './spotify';
+
 
 function Login() {
     return (
         <div className="login">
             
-            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
                  alt=""
             />
-            <a>LOGIN WITH SPOTIFY</a>
+            <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
