5d1b452a7e4bd8bf8bb6f40afd6533c0bc04a234
ravi yadav <raviyadav00063@gmail.com>
2020-12-18 19:39:42 +0530

    Spotify Icon is designed

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className="login">
+            
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
+                 alt=""
+            />
+            <a>LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
