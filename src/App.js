9d9d754237193249cad6831da86b0fceb294236e
ravi yadav <raviyadav00063@gmail.com>
2020-12-23 10:15:13 +0530

    UI design part implementation is in process

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import { useDataLayerValue } from "./DataLayer"
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-  const [{}, dispatch] = useDataLayerValue();
+  
+  const [{ user, token }, dispatch] = useDataLayerValue();
 
   //Run code based on a given condition
   useEffect(() => {
@@ -19,24 +19,36 @@ function App() {
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+
+      dispatch({
+        type:"SET_TOKEN",
+        token: _token,
+      })
+      
 
       spotify.setAccessToken(_token);
 
       spotify.getMe().then(user => {
-        console.log('Person', user);
+        
+
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
       })
     }
 
 
-    console.log("I HAVE A TOKEN ", token);
+   
   }, []);
+
+  
 
   return (
     <div className="App">
      {
        token ? (
-         <Player />
+         <Player spotify={spotify}/>
        ) : (
          <Login />
        )
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
+    //Remove after finish developing...
+    token: 'BQABibm_PPJoheX5QFwX0jyaJ5fuMOBFD85myuns_GAeX8Q1sV724yRgRDfNH4MX01BXaaUnWXyqVKnzqGNs3c-osU0eeQEHiBwoEpq3eMbgD2rNd9EKwsumFwpsqIGCHMfOg74t3K9idUh7V3ST-oOeFdjjpXCHkWeIk1tncVV0D3axvU_W',
 
 };
 
@@ -14,7 +16,13 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 user: action.user
-            }
+            };
+
+            case 'SET_TOKEN':
+                return {
+                    ...state,
+                    token: action.token
+                }
             default:
                 return state;
     }
